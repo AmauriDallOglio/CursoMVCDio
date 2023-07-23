2579dda43063b61aae867a5b37af5a1ad9d6cedf
Amauri DallOglio <amauridalloglio@outlook.com.br>
2023-07-23 20:27:00 -0300

    Adicionar arquivos de projeto.

---
+++ CursoMVCDio/Models/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CursoMVCDio.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString:@"Server=DESKTOP-Q37347P;Database=CURSO;Trusted_Connection=True;Encrypt=False");
+        }
+    }
+}
